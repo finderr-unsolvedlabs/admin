f1a7826b5cf9df38db797136da7691ebbfa62af0
Deepanshu Singh <58651615+sdeepanshu17@users.noreply.github.com>
2024-01-25 18:31:41 +0530

    table ui fixes, added sidebar to users page

--- src/components/Tables/CartProducts.tsx
+++ src/components/Tables/CartProducts.tsx
@@ -55,7 +55,7 @@ const CartProducts = ({ userDetails }: Props) => {
         <div className="relative overflow-x-auto">
           <table className="w-full text-sm text-left rtl:text-right text-gray-500">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-              <tr>
+              <tr className="border">
                 <th scope="col" className="px-6 py-3">
                   Product Name
                 </th>
--- src/components/Tables/UsersTable.tsx
+++ src/components/Tables/UsersTable.tsx
@@ -38,7 +38,7 @@ const UsersTable = ({ userList }: Props) => {
       <div className="relative overflow-x-auto">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-            <tr>
+            <tr className="border">
               <th scope="col" className="px-6 py-3">
                 User Name
               </th>
