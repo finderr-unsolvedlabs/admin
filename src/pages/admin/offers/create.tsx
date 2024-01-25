211915cc185a52d048b689f67efe2f5fa0ab398a
Deepanshu Singh <58651615+sdeepanshu17@users.noreply.github.com>
2024-01-25 10:40:34 +0530

    added create event, form functionality

--- src/pages/admin/offers/create.tsx
+++ src/pages/admin/offers/create.tsx
@@ -1,6 +1,9 @@
 import { SidebarLayout } from "@/components/Layout/SidebarLayout";
 import { AdminSidebar } from "@/components/Sidebar/AdminSidebar";
-import React from "react";
+import { OffersApi } from "@/services/api/offers";
+import { ICreateOfferForm } from "@/services/interfaces/forms";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 const create = () => {
   return (
@@ -11,19 +14,55 @@ const create = () => {
   );
 };
 
+export const emptyFormData: ICreateOfferForm = {
+  title: "",
+  imageKey: "",
+  expiry_date: "",
+  brand_id: "",
+  action: {
+    label: "",
+    url: "",
+  },
+};
+
 const Main = () => {
+  const router = useRouter();
+
+  const [formData, setFormData] = useState<ICreateOfferForm>(emptyFormData);
+
+  const submitHandler = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const dateParts = formData.expiry_date.split("-");
+    const expiry_date_formatted = `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`;
+    formData.expiry_date = expiry_date_formatted;
+
+    OffersApi.createOffer(formData)
+      .then(({ data }) => {
+        alert("Offer Added Successfully");
+        router.push("/admin/offers");
+      })
+      .catch((error) => {
+        alert("Something went wrong!");
+        console.error(error);
+      });
+  };
+
   return (
     <div className="flex flex-col gap-2 p-4 w-full h-full">
       <h1 className="text-2xl font-semibold">Add New Offer</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="flex justify-center gap-2">
           <div className="mt-5 w-full">
             <label className="block mb-2 text-sm font-medium text-gray-900">
               Title*
             </label>
             <input
-              id="title"
+              id="e_title"
               type="text"
+              onChange={(e) =>
+                setFormData({ ...formData, title: e.target.value })
+              }
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Title"
               required
@@ -36,6 +75,9 @@ const Main = () => {
             <input
               id="expiry_date"
               type="date"
+              onChange={(e) =>
+                setFormData({ ...formData, expiry_date: e.target.value })
+              }
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Select date"
               required
@@ -50,6 +92,12 @@ const Main = () => {
             <input
               id="url_label"
               type="text"
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  action: { label: e.target.value, url: formData.action.url },
+                })
+              }
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Link Label"
               required
@@ -62,6 +110,12 @@ const Main = () => {
             <input
               id="url"
               type="text"
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  action: { url: e.target.value, label: formData.action.label },
+                })
+              }
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Url"
               required
@@ -76,6 +130,9 @@ const Main = () => {
             <input
               id="image_s3_key"
               type="text"
+              onChange={(e) =>
+                setFormData({ ...formData, imageKey: e.target.value })
+              }
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="image s3 key"
               required
@@ -92,16 +149,33 @@ const Main = () => {
               placeholder="logo s3 key (optional)"
             />
           </div>
+          <div className="mt-5 w-full">
+            <label className="block mb-2 text-sm font-medium text-gray-900">
+              Brand Id
+            </label>
+            <input
+              id="brand_id"
+              type="text"
+              onChange={(e) =>
+                setFormData({ ...formData, brand_id: e.target.value })
+              }
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder="brand_id (optional)"
+            />
+          </div>
         </div>
         <div className="mt-5">
           <label className="block mb-2 text-sm font-medium text-gray-900">
-            Description*
+            Description
           </label>
           <textarea
             id="desc"
             rows={4}
+            onChange={(e) =>
+              setFormData({ ...formData, description: e.target.value })
+            }
+            placeholder="Description (optional)"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            required
           />
         </div>
         <div className="flex justify-center mt-5">
--- src/services/api/offers.ts
+++ src/services/api/offers.ts
@@ -1,6 +1,7 @@
 import { AxiosResponse } from "axios";
 import { sendRequest } from ".";
 import { IOfferListResponse } from "../interfaces/common";
+import { ICreateOfferForm } from "../interfaces/forms";
 
 const baseUrl = `/admin/offers`;
 
@@ -8,5 +9,11 @@ const list = (page: number): Promise<AxiosResponse<IOfferListResponse>> => {
   return sendRequest(`${baseUrl}?limit=10&page=${page}`);
 };
 
-const OffersApi = { list };
+const createOffer = (
+  form: ICreateOfferForm
+): Promise<AxiosResponse<string>> => {
+  return sendRequest(`${baseUrl}/create`, { method: "POST", data: form });
+};
+
+const OffersApi = { list, createOffer };
 export { OffersApi };
