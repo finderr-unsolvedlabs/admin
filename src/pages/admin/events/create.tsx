9e2f6f2fc298870cda71b1ef07a86bd78571b473
Deepanshu Singh <58651615+sdeepanshu17@users.noreply.github.com>
2024-01-25 10:40:34 +0530

    added create event, offer functionality

--- src/pages/admin/events/create.tsx
+++ src/pages/admin/events/create.tsx
@@ -1,5 +1,8 @@
 import { SidebarLayout } from "@/components/Layout/SidebarLayout";
 import { AdminSidebar } from "@/components/Sidebar/AdminSidebar";
+import { EventsApi } from "@/services/api/events";
+import { ICreateEventForm } from "@/services/interfaces/forms";
+import { useRouter } from "next/router";
 import React from "react";
 
 const create = () => {
@@ -11,19 +14,55 @@ const create = () => {
   );
 };
 
+export const emptyFormData: ICreateEventForm = {
+  title: "",
+  imageKey: "",
+  expiry_date: "",
+  action: {
+    label: "",
+    url: "",
+  },
+};
+
 const Main = () => {
+  const router = useRouter();
+
+  const [formData, setFormData] =
+    React.useState<ICreateEventForm>(emptyFormData);
+
+  const submitHandler = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const dateParts = formData.expiry_date.split("-");
+    const expiry_date_formatted = `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`;
+    formData.expiry_date = expiry_date_formatted;
+
+    EventsApi.createEvent(formData)
+      .then(({ data }) => {
+        alert("Event Created Successfully");
+        router.push("/admin/events");
+      })
+      .catch((error) => {
+        alert("Something went wrong!");
+        console.error(error);
+      });
+  };
+
   return (
     <div className="flex flex-col gap-2 p-4 w-full h-full">
       <h1 className="text-2xl font-semibold">Add New Event</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="flex justify-center gap-2">
           <div className="mt-5 w-full">
             <label className="block mb-2 text-sm font-medium text-gray-900">
               Title*
             </label>
             <input
-              id="title"
+              id="e_title"
               type="text"
+              onChange={(e) =>
+                setFormData({ ...formData, title: e.target.value })
+              }
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Title"
               required
@@ -36,6 +75,9 @@ const Main = () => {
             <input
               id="expiry_date"
               type="date"
+              onChange={(e) =>
+                setFormData({ ...formData, expiry_date: e.target.value })
+              }
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Select date"
               required
@@ -50,6 +92,12 @@ const Main = () => {
             <input
               id="url_label"
               type="text"
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  action: { label: e.target.value, url: formData.action.url },
+                })
+              }
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Link Label"
               required
@@ -62,6 +110,12 @@ const Main = () => {
             <input
               id="url"
               type="text"
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  action: { url: e.target.value, label: formData.action.label },
+                })
+              }
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Url"
               required
@@ -76,6 +130,9 @@ const Main = () => {
             <input
               id="image_s3_key"
               type="text"
+              onChange={(e) =>
+                setFormData({ ...formData, imageKey: e.target.value })
+              }
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="image s3 key"
               required
@@ -95,13 +152,16 @@ const Main = () => {
         </div>
         <div className="mt-5">
           <label className="block mb-2 text-sm font-medium text-gray-900">
-            Description*
+            Description
           </label>
           <textarea
             id="desc"
             rows={4}
+            onChange={(e) =>
+              setFormData({ ...formData, description: e.target.value })
+            }
+            placeholder="Description (optional)"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            required
           />
         </div>
         <div className="flex justify-center mt-5">
--- src/services/api/events.ts
+++ src/services/api/events.ts
@@ -1,6 +1,7 @@
 import { AxiosResponse } from "axios";
 import { sendRequest } from ".";
 import { IEventListResponse } from "../interfaces/common";
+import { ICreateEventForm } from "../interfaces/forms";
 
 const baseUrl = `/admin/events`;
 
@@ -8,5 +9,11 @@ const list = (page: number): Promise<AxiosResponse<IEventListResponse>> => {
   return sendRequest(`${baseUrl}?limit=10&page=${page}`);
 };
 
-const EventsApi = { list };
+const createEvent = (
+  form: ICreateEventForm
+): Promise<AxiosResponse<string>> => {
+  return sendRequest(`${baseUrl}/create`, { method: "POST", data: form });
+};
+
+const EventsApi = { list, createEvent };
 export { EventsApi };
