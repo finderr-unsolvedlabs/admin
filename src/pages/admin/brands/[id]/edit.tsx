209fbdba0eeaf0eecfa9e1ac5c08c5737fc6485f
AayushOjha <ayush2ojha22ayush@gmail.com>
2024-02-09 10:53:45 +0530

    Cover Images not required

--- src/pages/admin/brands/[id]/edit.tsx
+++ src/pages/admin/brands/[id]/edit.tsx
@@ -214,7 +214,7 @@ const Main = ({ brandData }: Props) => {
         <div className="flex justify-center gap-2">
           <div className="mt-5 w-3/5">
             <label className="block mb-2 text-sm font-medium text-gray-900">
-              Cover Images Keys (comma separated)*
+              Cover Images Keys (comma separated)
             </label>
             <input
               id="logo_key"
@@ -223,7 +223,6 @@ const Main = ({ brandData }: Props) => {
               onChange={(e) => setCoverImages(e.target.value)}
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Cover Images s3 key"
-              required
             />
           </div>
           <div className="mt-5 w-2/5">
